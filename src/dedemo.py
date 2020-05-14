cae9037606e993271fd7ce8f636fe02d6041b39f
ivoripuion <1049971327@qq.com>
2020-05-14 18:07:26 +0800

    update

--- src/dedemo.py
+++ src/dedemo.py
@@ -3,7 +3,7 @@ import pefile
 import binascii
 import rsa
 
-pe_path=".\\file_to_write1.exe"
+pe_path=".\\file_to_write2.exe"
 pe=pefile.PE(pe_path)
 msg_length=128
 
@@ -17,9 +17,18 @@ def get_addr():
             data_addr=section.VirtualAddress-128
             return data_addr
     return -1
-            
-if __name__=="__main__":
 
+def get_addr2():
+    for section in pe.sections:
+        if section.Name==b'.data\x00\x00\x00':
+            data_addr1=section.VirtualAddress-64
+            
+        if section.Name==b'.rdata\x00\x00':
+            data_addr2=section.VirtualAddress-64
+
+    return [data_addr1,data_addr2]
+
+def detest1():
     addr=get_addr()
 
     msg=pe.get_data(addr,msg_length)
@@ -28,3 +37,19 @@ if __name__=="__main__":
 
     print("inseted data: ",message)
 
+
+def detest2():
+    addr1=get_addr2()[0]
+    addr2=get_addr2()[1]
+
+    print("data1:",hex(addr1))
+    print("data2",hex(addr2))
+    data1=pe.get_data(addr1,64)
+    data2=pe.get_data(addr2,64)
+    data=data1+data2
+    message = rsa.decrypt(data, privkey).decode()
+    print("inseted data: ",message)
+
+if __name__=="__main__":
+
+    detest2()
--- src/demo.py
+++ src/demo.py
@@ -45,7 +45,7 @@ def find_avaliable_addr(msg):
     print("address ",hex(addr),"is avaliable!")
     return addr
     
-#插入秘密信息
+#插入秘密信息到单一段
 def insert_msg(addr,msg):
     if addr!=-1:
         index=0
@@ -62,8 +62,59 @@ def insert_msg(addr,msg):
         print("address is not valid")
     
 
+#找出存放拆分开的秘密信息的地址
+def find_avaliable_addr2(msg):
+    msg1=msg[:64]
+    msg2=msg[64:]
 
-if __name__=="__main__":
+    for section in pe.sections:
+        if section.Name==b'.data\x00\x00\x00':    
+            data1_rva=section.VirtualAddress-64
+            print("find avaliable address blow .data address:",hex(data1_rva))
+        
+        if section.Name==b'.rdata\x00\x00':    
+            data2_rva=section.VirtualAddress-64
+            print("find avaliable address blow .text address:",hex(data2_rva))
+
+    return [data1_rva,data2_rva]
+
+#插入秘密信息到多个段
+def insert_msg2(addr1,addr2,msg):
+    msg1=msg[:64]
+    msg2=msg[64:]
+
+    #塞msg1
+    index1=0
+    for char1 in msg1:
+        pe.set_dword_at_rva(addr1+index1,char1)
+        index1+=1
+
+     #塞msg2
+    index2=0
+    for char2 in msg2:
+        pe.set_dword_at_rva(addr2+index2,char2)
+        index2+=1
+
+    print("insert data into .data & .rdata")
+    pe.write(filename='.\\file_to_write2.exe')
+
+
+def test1():
     encrypted_msg=encrypt(raw_msg)
+    
+    #嵌入到单一段
     addr=find_avaliable_addr(encrypted_msg)
     insert_msg(addr,encrypted_msg)
+
+def test2():
+    encrypted_msg=encrypt(raw_msg)
+
+    #嵌入到多个段
+    addr=find_avaliable_addr2(encrypted_msg)
+    addr1=addr[0]
+    addr2=addr[1]
+    insert_msg2(addr1,addr2,encrypted_msg)
+
+if __name__=="__main__":
+    
+    test2()
